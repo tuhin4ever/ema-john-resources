e442e5004f9adf6ab5f0428fc195562b6d69bc4e
tuhin_4ever <tuhin4ever19@gmail.com>
2023-03-29 00:32:09 +0600

    show data from cart

--- ema-jhon-simple/src/components/Cart/Cart.jsx
+++ ema-jhon-simple/src/components/Cart/Cart.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import "./Cart.css";
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
   //   const cart = props.cart; //option 1
   // const {cart} = props; //option 2
   console.log(cart);
 
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
 
-  for(const product of cart){
-    totalPrice = totalPrice + product.price;
+  for (const product of cart) {
+    
+    // product.quantity = product.quantity || 1; //* option 1
+
+    // if(product.quantity === 0){
+    //     product.quantity = 1;
+    // } //* option 2
+    
+    totalPrice = totalPrice + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
+
   }
-  const tax = totalPrice * 7 /100;
+  const tax = (totalPrice * 7) / 100;
   const grandTotal = totalPrice + totalShipping + tax;
 
   return (
     <div className="cart">
       <h4>Oder Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Total Shipping: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
--- ema-jhon-simple/src/components/Shop/Shop.jsx
+++ ema-jhon-simple/src/components/Shop/Shop.jsx
@@ -28,16 +28,31 @@ const Shop = () => {
         const quantity = storedCart[id];
         addedProduct.quantity = quantity;
         // step 4: add the added product to the saved cart
-        savedCart.push(addedProduct)
+        savedCart.push(addedProduct);
       }
       // console.log("added product", addedProduct);
     }
     // step 5: set the cart
-    setCart(savedCart)
+    setCart(savedCart);
   }, [products]);
 
   const handleAddToCart = (product) => {
-    const newCart = [...cart, product];
+    // cart.push(product);
+    let newCart = [];
+    // const newCart = [...cart, product];
+    //* option 3 Super advanced
+    //* if product doesn't exist in the cart, then set quantity = 1
+    //* it exist update quantity by 1
+    const exists = cart.find((pd) => pd.id === product.id);
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    } else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter((pd) => pd.id !== product.id);
+      newCart = [...remaining, exists];
+    }
+
     setCart(newCart);
     addToDb(product.id);
   };
